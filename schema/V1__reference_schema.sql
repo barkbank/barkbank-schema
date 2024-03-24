79d860cae29c10b04b8b8941ac3d642c6328a0ec
Jin <hsienjin@gmail.com>
2024-03-24 08:42:54 +0800

    dogs-view - evalaute conditions for permanent ineligibility before all other conditions

--- schema/V1__reference_schema.sql
+++ schema/V1__reference_schema.sql
@@ -108,10 +108,10 @@ CREATE TYPE t_dog_status AS ENUM (
 CREATE VIEW dogs_view AS (
   SELECT
     CASE
-      WHEN tDog.dog_breed = '' AND tDog.dog_weight_kg IS NULL THEN 'INCOMPLETE'::t_dog_status
-      WHEN tDog.dog_weight_kg < 20 THEN 'INELIGIBLE'::t_dog_status
       WHEN tDog.dog_ever_pregnant = 'YES' THEN 'PERMANENTLY_INELIGIBLE'::t_dog_status
       WHEN tDog.dog_ever_received_transfusion = 'YES' THEN 'PERMANENTLY_INELIGIBLE'::t_dog_status
+      WHEN tDog.dog_breed = '' AND tDog.dog_weight_kg IS NULL THEN 'INCOMPLETE'::t_dog_status
+      WHEN tDog.dog_weight_kg < 20 THEN 'INELIGIBLE'::t_dog_status
       ELSE 'ELIGIBLE'::t_dog_status
     END as "dog_status",
     tDog.*
