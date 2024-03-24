f1722bfbe96cf5bb628f67f2d9fbbfda01ff6fbd
Jin <hsienjin@gmail.com>
2024-03-24 09:46:47 +0800

    create-table-calls - create table calls and t_call_outcome enum

--- schema/V1__reference_schema.sql
+++ schema/V1__reference_schema.sql
@@ -95,6 +95,18 @@ CREATE TABLE dog_vet_preferences (
   CONSTRAINT dog_vet_preferences_pk PRIMARY KEY (dog_id, vet_id)
 );
 
+CREATE TYPE t_call_outcome AS ENUM ('APPOINTMENT', 'DECLINED', 'OPT_OUT');
+
+CREATE TABLE calls (
+  call_id BIGSERIAL,
+  call_creation_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  vet_id BIGINT NOT NULL,
+  dog_id BIGINT NOT NULL,
+  call_outcome t_call_outcome NOT NULL,
+  encrypted_opt_out_reason TEXT NOT NULL,
+  CONSTRAINT calls_pk PRIMARY KEY (call_id)
+);
+
 -- ------------------------------------------------------------
 -- # Triggers
 
