ea43395b0ca779bef6feabfa2f15537a305398c3
Jin <hsienjin@gmail.com>
2024-05-19 00:01:25 +0800

    enable-row-level-security - set security_invoker=on option for reference schema

--- scripts/list-missing-rls.sql
+++ scripts/list-missing-rls.sql
@@ -5,15 +5,24 @@ SELECT
         THEN 'table'
         ELSE 'view'
     END as "rel_type",
-    cls.relname as "rel_name",
-    cls.relrowsecurity as "rls_enabled"
+    cls.relname as "rel_name"
 FROM pg_namespace as ns
 LEFT JOIN pg_class as cls on ns.oid = cls.relnamespace
 WHERE ns.nspname = 'public'
-AND (
-    cls.relname IN (SELECT tablename FROM pg_tables)
-    OR cls.relname IN (SELECT viewname FROM pg_views)
-)
-AND cls.relrowsecurity = false
 AND cls.relname <> 'flyway_schema_history'
-ORDER BY namespace, rel_type, rel_name
+AND (
+    (
+        -- Is a table without RLS enabled
+        cls.relname IN (SELECT tablename FROM pg_tables)
+        AND cls.relrowsecurity = false
+    )
+    OR (
+        -- Is a view without security_invoker=on
+        cls.relname IN (SELECT viewname FROM pg_views)
+        AND (
+            cls.reloptions IS NULL
+            OR array_position(cls.reloptions, 'security_invoker=on') IS NULL
+        )
+    )
+)
+ORDER BY "namespace", "rel_type", "rel_name"
