317f3fcd0de80aa634d940c3de26dc586f641e99
Jin <hsienjin@gmail.com>
2024-09-14 09:19:30 +0800

    SCHEMA-12: Migration script to add pawtal_version column

---
+++ migrations/V51__add_pawtal_version.sql
@@ -0,0 +1,18 @@
+ALTER TABLE pawtal_events
+ADD COLUMN pawtal_version TEXT DEFAULT NULL;
+
+-- (1) It is necessary to recreate view_pawtal_events to include the new column.
+-- This is because the `*` expression is resolved and at the time the CREATE
+-- VIEW statement is executed and the columns included by the view is
+-- consequently fixed at the point in time.
+--
+-- (2) CREATE OR REPLACE VIEW does not work in this case. Hence the view is
+-- dropped and recreated.
+DROP VIEW view_pawtal_events;
+CREATE VIEW view_pawtal_events
+WITH (security_invoker=on)
+AS
+SELECT
+  *,
+  (event_ts AT TIME ZONE 'Asia/Singapore')::date AS "day"
+FROM pawtal_events;
